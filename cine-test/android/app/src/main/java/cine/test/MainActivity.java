bc718cf8db5c84c8532bcb27d1f4158caeb2cdba
Vinícius <vinidarthbarbosa03@gmail.com>
2025-10-24 21:05:58 -0300

    Colocando o Projeto IONIC

---
+++ cine-test/android/app/src/main/java/cine/test/MainActivity.java
@@ -0,0 +1,5 @@
+package cine.test;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ cine-test/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'cine.test',
+  appName: 'cine-test',
+  webDir: 'www'
+};
+
+export default config;
